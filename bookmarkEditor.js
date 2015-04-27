9bf7b9c1366b757e17ddb298374e42acdffa2437
unknown <vgunning@mit.edu>
2015-04-26 21:54:15 -0400

    First test page commit

---
+++ bookmarkEditor.js
@@ -0,0 +1,195 @@
+
+	
+	//Make all button declarations here or before!
+	var bookmark = $('#btnBookmarkEdit');
+	addBookmarkEditorFunctionality(bookmark);
+	var bookmark2 = $('#btnBookmarkEdit2');
+	addBookmarkEditorFunctionality(bookmark2);
+	///////////////////////////////////////////////
+	
+	var SEMAPHORE = 0; 	
+	var bookmarkId;
+	
+	function addBookmarkEditorFunctionality(bookmark){
+		
+		bookmark.click(function(e) {
+		    
+		    e.stopPropagation();
+		    var caller = $(e.currentTarget.offsetParent.offsetParent);
+		    
+		    // console.log(bookmarkId);
+
+		    if (caller[0].getAttribute("data-clicked") != "true" && SEMAPHORE==0){
+	    		console.log(caller);
+	    		popBookmarkEditor(caller);
+	    		caller[0].setAttribute("data-clicked","true");
+	    		SEMAPHORE = 1;
+	    		bookmarkId = caller[0];
+		    	
+		    }else {
+		    	if (bookmarkId==caller[0]){
+		    		$('#noteContainer').remove(); 
+		    		caller[0].removeAttribute("data-clicked");
+		    		SEMAPHORE = 0;
+		    	}
+		    	
+		    }
+		    
+
+			$( "#note" )
+	        	.resizable({
+	      			maxWidth: 250,
+	      			maxHeight: 500,
+	      			minHeight: 315,
+	      			minWidth: 200
+	    		})
+	        	.draggable();
+
+	        //When the bookmark name is double clicked, it becomes at text field that has the name of the bookmark selected
+		    $('#bookmarkName').dblclick(function() {
+			    $('#bookmarkName').css('display', 'none');
+			    $('#bookmarkName_entry')
+			        .val($('#bookmarkName').text())
+			        .css('display', '')
+			        .select()
+			        .focus();
+			});
+
+		    //When the bookmark input field looses focus, it changes the name of the bookmark (if the name is not empty)
+			$('#bookmarkName_entry').blur(function() {
+			    $('#bookmarkName_entry').css('display', 'none');
+		        if (checkEmpty("bookmarkName_entry")){
+			    	$('#bookmarkName').text($('#bookmarkName_entry').val());
+			    }
+			        $('#bookmarkName').css('display', '');
+			});
+
+			//When enter is pressed on the bookmark name input, it changes the name of the bookmark (if the name is not empty)
+			$("#bookmarkName_entry").keypress(function(event) {
+		    //13 is the key code for the enter key.
+		    if (event.which == 13) {
+		        event.preventDefault();
+		        $('#bookmarkName_entry').css('display', 'none');
+		        if (checkEmpty("bookmarkName_entry")){
+			    	$('#bookmarkName').text($('#bookmarkName_entry').val());
+			    }
+			        $('#bookmarkName').css('display', '');
+			    $( "#btnDone" ).focus();
+		                                                                      
+		    }
+		    });
+
+		    //When the done button is clicked, the name in the caller button is changed and the editor widget is closed
+		    $( "#btnDone" ).click(function(){
+				if (checkEmpty("bookmarkName_entry")){
+					caller[0].firstChild.innerHTML = ($('#bookmarkName_entry').val());
+				}
+				$('#noteContainer').remove();
+				caller[0].removeAttribute("data-clicked");
+				SEMAPHORE = 0;
+				//caller.prop('disabled', false);
+		    });
+
+		    //The editor widget is closed without changing anything in the bookmark.
+		    $( "#btnCancel" ).click(function(){
+				$('#noteContainer').remove();
+				caller[0].removeAttribute("data-clicked");
+				SEMAPHORE = 0;
+				//caller.prop('disabled', false);
+
+		    });
+
+	    //caller.prop('disabled', true);	    
+
+	});
+	}
+	
+ 	
+
+/**
+  * Returns true if the value of element_string is not empty.
+  * @param {Node} element_string an input html element that contains the text we want to check
+  * Code taken from http://stackoverflow.com/questions/3502354/how-to-check-if-a-textbox-is-empty-using-javascript and modified afterwards.*
+  */
+function checkEmpty(element_string) { 
+	var myString = document.getElementById(element_string).value; 
+    if(!myString.match(/\S/)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+/**
+  * Creates a bookmark note widget. For now it adds it to the end of the html. Ideally we can use the information of the position of the caller
+  * to make it appear right next to it.
+  * @param {$(Node)} caller is a jquery element that contains the button (or the container of the button) that calls the widget. 
+  *					Its text must be the name of the bookmark. 
+  */
+function popBookmarkEditor(caller) {     	
+	var bookmarkName = caller.text();
+	var rect = caller.offset();
+
+	var bookmarkTop = rect.top - 10;
+	var bookmarkLeft = rect.left + caller.outerWidth() + caller.children('.list-group-submenu').outerWidth();
+
+	console.log(caller.children('.list-group-submenu').outerWidth());
+
+	var bookmarkEditorContainer = document.createElement("div");
+	var bookmarkEditor = document.createElement("div");
+	var bookmarkNameContainerParent = document.createElement("ul");
+	var bookmarkNameContainer = document.createElement("li");
+	var bookmarkNameLabel = document.createElement("a");
+	var bookmarkNameEditor = document.createElement("input");
+	var noteContainer = document.createElement("div");
+	var note = document.createElement("textarea");
+	var cancelButton = document.createElement("button");
+	var doneButton = document.createElement("button");  	
+
+	bookmarkEditorContainer.setAttribute("id","noteContainer");
+	bookmarkEditorContainer.style.position = "absolute";
+	$(bookmarkEditorContainer).offset({ top: bookmarkTop, left: bookmarkLeft});
+	console.log(bookmarkLeft);
+	console.log(bookmarkEditorContainer.style.left);
+	
+	bookmarkEditor.setAttribute("id","note");
+	$(bookmarkEditor).addClass("effect1");
+
+	bookmarkNameContainerParent.setAttribute("class","nav nav-pills nav-stacked")
+	
+	bookmarkNameContainer.setAttribute("id","bookmarkNameContainer")
+	bookmarkNameContainer.setAttribute("class","active indigo")
+	
+	bookmarkNameLabel.setAttribute("id","bookmarkName");
+	bookmarkNameLabel.innerHTML =  bookmarkName;
+
+	bookmarkNameEditor.setAttribute("id","bookmarkName_entry");
+	bookmarkNameEditor.style.display = "none";
+
+	noteContainer.setAttribute("class","center");
+	
+	note.setAttribute("id","text");
+
+	cancelButton.innerHTML = "Cancel";
+	cancelButton.setAttribute("id","btnCancel");
+	cancelButton.setAttribute("class","btn");
+
+
+	doneButton.innerHTML = "Done";
+	doneButton.setAttribute("id","btnDone");
+	doneButton.setAttribute("class","btn");
+
+	noteContainer.appendChild(note);
+	bookmarkNameContainer.appendChild(bookmarkNameLabel);
+	bookmarkNameContainer.appendChild(bookmarkNameEditor);
+	bookmarkNameContainerParent.appendChild(bookmarkNameContainer);
+	
+	bookmarkEditor.appendChild(bookmarkNameContainerParent);
+	bookmarkEditor.appendChild(noteContainer);
+	bookmarkEditor.appendChild(cancelButton);
+	bookmarkEditor.appendChild(doneButton);
+	bookmarkEditorContainer.appendChild(bookmarkEditor)
+	document.body.appendChild(bookmarkEditorContainer);
+}
+
+	
---
+++ objects.js
@@ -0,0 +1,125 @@
+/////////////////////////////////////// I think this is the proper way
+// playlist javascript doesn't have constructor overloading
+function Playlist(){
+	this.type = 'playlist';
+}
+Playlist.prototype = {
+	// all the prototypes functions
+	addClip: function(newClip){
+		//Should we check for the existence of the clip in the clip list?. What would clip equality be in that case?
+		// How does javascript take care of this? I will just check for the name for now. -X
+		var canAdd = true;
+		for (clip in this.clips){
+			if (clip.name == newClip.name){
+				canAdd = false;
+			}
+		} 
+		if (canAdd) this.clips.push(newClip);
+	},
+
+	removeClip: function(toRemoveClip){
+		var index = this.clips.indexOf(toRemoveClip);
+		if (index>-1){
+    		this.clips.splice(index, 1);
+		}
+	}
+}
+// constructor with just the name
+Playlist.prototype.init_name = function(name){
+	this.name = name;
+	this.id = name.split('-').join('').split(' ').join('');
+	this.nospace = name.split('-').join('').split(' ').join('');
+	this.clips = [];
+	return this;
+}
+
+// clip object
+function Clip(){
+	this.type = 'clip';
+}
+
+Clip.prototype = {
+	// declare all the functions that clip should support to inherit
+	addBookmark: function(newBookmark){
+		//Should we check for the existence of the clip in the clip list?. What would clip equality be in that case?
+		// How does javascript take care of this? I will just check for the name for now. -X
+		var canAdd = true;
+		for (bookmark in this.bookmarks){
+			if (bookmark.name == newBookmark.name){
+				canAdd = false;
+			}
+		} 
+		if (cannAdd) this.bookmarks.push(newBookmark);
+	},
+
+	removeBookmark: function(toRemoveBookmark){
+		var index = this.clips.indexOf(toRemoveBookmark);
+		if (index>-1){
+    		this.bookmarks.splice(index, 1);
+		}
+	}
+};
+
+Clip.prototype.init_name = function(name){
+	this.name = name;
+	this.bookmarks = [];
+	return this;
+}
+
+Clip.prototype.init_name_playlist = function(name, playlist){
+	this.name = name;
+	this.playlist = playlist; //This is the parent/container playlist.
+	//This can possibly be the id that will be given to the corresponding html tag: P<playlist name>C<clip name>B<bookmark name>
+	this.id = this.playlist.id + '-' + name.split('-').join('').split(' ').join('');
+	this.nospace = name.split('-').join('').split(' ').join('');
+	this.bookmarks = [];
+	return this;
+}
+
+function Bookmark(){
+	this.type = 'bookmark';
+	this.text = '';
+	this.startTime = -1; //In milliseconds
+	this.endTime = -1; //In milliseconds
+}
+
+Bookmark.prototype = {
+	addText: function(newText){
+		this.text = newText;
+	},
+
+	removeText: function(){
+		this.text = "";
+	}
+}
+
+Bookmark.prototype.init_name = function(name){
+	this.name = name;
+	return this;
+}
+
+Bookmark.prototype.init_name_times = function(name, startTime, endTime){
+	this.name = name;
+	this.nospace = name.split('-').join('').split(' ').join('');
+	this.startTime = startTime;
+	this.endTime = endTime;
+	return this;
+}
+
+Bookmark.prototype.init_name_clip = function(name, clip){
+	this.name = name;
+	this.nospace = name.split('-').join('').split(' ').join('');
+	this.clip = clip;
+	//This can possibly be the id that will be given to the corresponding html tag: P<playlist name>C<clip name>B<bookmark name>
+	this.id = this.clip.id + '-' + name.split('-').join('').split(' ').join('');
+	return this;
+}
+
+Bookmark.prototype.init_name_clip_playlist = function(name, clip, playlist){
+	this.name = name;
+	this.nospace = name.split('-').join('').split(' ').join('');
+	this.clip = clip;
+	this.playlist = playlist;
+	this.id = this.playlist.id + '-' + this.clip.id + '-' + name.split('-').join('').split(' ').join('');
+	return this;
+}
