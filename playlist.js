0a70b29c03b7220f1bb3e117ced68201c1721710
unknown <vgunning@mit.edu>
2015-05-05 15:21:02 -0400

    fixed 'Enter' on the playlist creation dialog

--- playlist.js
+++ playlist.js
@@ -48,34 +48,37 @@ function newPlaylist(){
 }
 
 function savePlaylists(){
-	$('#newPlaylistWindow').modal('hide'); // close the dialog box
-	var clipsToAdd = document.getElementById('added-container').getElementsByClassName('btn'); // id has the clip name?
-	var playlistName = document.getElementById('recipient-name').value;
-	if (playlistName == ''){
-		playlistName = 'Playlist ' + (playlists.length + 1).toString();
-	}
-	// check to see if that playlist name already exists
-	if (isPlaylistUsed(playlistName)){
-		playlistName = playlistName + ' (1)';
-	}
-	document.getElementById('recipient-name').value = '';
-	var playlist = new Playlist().init_name(playlistName);
-	for (var i = 0; i < clipsToAdd.length; i++){
-		var clip = new Clip().init_name_playlist(clipsToAdd[i].textContent, playlist);
-		clip.addSrc('music/' + clip.name + '.mp3');
-		playlist.addClip(clip);
-	}
-	// add a new playlist for now
-	// checking should be implemented
-	playlists.push(playlist);
-	setCurrentPlaylist(playlists.length - 1);
-	if (clipsToAdd.length > 0){
-		setCurrentClip(0);
-	}
-	updateMenus();
-	makeActive(document.getElementById(playlists[playlists.length - 1].id));
-	if (clipsToAdd.length > 0){
-		makeActive(document.getElementById(playlists[playlists.length - 1].clips[0].id));
+	if ($('#newPlaylistWindow').hasClass('in')){
+		// this means the the dialog was actually open and to carry out the save action
+		$('#newPlaylistWindow').modal('hide'); // close the dialog box
+		var clipsToAdd = document.getElementById('added-container').getElementsByClassName('btn'); // id has the clip name?
+		var playlistName = document.getElementById('recipient-name').value;
+		if (playlistName == ''){
+			playlistName = 'Playlist ' + (playlists.length + 1).toString();
+		}
+		// check to see if that playlist name already exists
+		if (isPlaylistUsed(playlistName)){
+			playlistName = playlistName + ' (1)';
+		}
+		document.getElementById('recipient-name').value = '';
+		var playlist = new Playlist().init_name(playlistName);
+		for (var i = 0; i < clipsToAdd.length; i++){
+			var clip = new Clip().init_name_playlist(clipsToAdd[i].textContent, playlist);
+			clip.addSrc('music/' + clip.name + '.mp3');
+			playlist.addClip(clip);
+		}
+		// add a new playlist for now
+		// checking should be implemented
+		playlists.push(playlist);
+		setCurrentPlaylist(playlists.length - 1);
+		if (clipsToAdd.length > 0){
+			setCurrentClip(0);
+		}
+		updateMenus();
+		makeActive(document.getElementById(playlists[playlists.length - 1].id));
+		if (clipsToAdd.length > 0){
+			makeActive(document.getElementById(playlists[playlists.length - 1].clips[0].id));
+		}
 	}
 }
 
